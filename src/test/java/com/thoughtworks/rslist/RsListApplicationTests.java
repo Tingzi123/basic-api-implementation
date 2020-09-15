32c8e9f3d64b3c54402376c09932567a634cb30b
Tingzi123 <1044373556@qq.com>
2020-09-15 19:41:25 +0800

    [chenting]:add function get rs list

--- src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
+++ src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
@@ -1,13 +1,35 @@
 package com.thoughtworks.rslist;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class RsListApplicationTests {
 
+    @Autowired
+    MockMvc mockMvc;
+
     @Test
-    void contextLoads() {
+    void contextLoads() throws Exception {
+        //get("rs/list")
+        /*MvcResult mvcResult = mockMvc.perform(get("rs/list")).andreturn();
+        MockHttpServletResponse response=mvcResuit.getResponse();
+        int status=response.getStatus();
+        assertEquals(200,status);
+        assertEquals(response.getContentString(),"[第一条事件, 第二条事件, 第三条事件]");*/
+
+        mockMvc.perform(get("/rs/list"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("[第一条事件, 第二条事件, 第三条事件]"));
     }
 
 }
