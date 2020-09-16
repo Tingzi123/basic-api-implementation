e072c035e3083a154f9d8d425c6cd5c01b83a3a5
Tingzi123 <1044373556@qq.com>
2020-09-16 15:51:47 +0800

    [chenting]:impiement user register

--- src/main/java/com/thoughtworks/rslist/dto/UserDto.java
+++ src/main/java/com/thoughtworks/rslist/dto/UserDto.java
@@ -1,4 +1,63 @@
 package com.thoughtworks.rslist.dto;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class UserDto {
+    private String name;
+    private String gender;
+    private Integer age;
+    private String email;
+    private String phone;
+    private Integer vote = 10;
+
+    public UserDto(String name, String gender, Integer age, String email, String phone) {
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
