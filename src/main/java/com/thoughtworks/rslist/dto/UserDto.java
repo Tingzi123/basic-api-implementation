e6bc2f7b5643516a6e5bb8622e1a1c920e80940f
Tingzi123 <1044373556@qq.com>
2020-09-16 17:19:26 +0800

    [chenting]:implement age is less than 18

--- src/main/java/com/thoughtworks/rslist/dto/UserDto.java
+++ src/main/java/com/thoughtworks/rslist/dto/UserDto.java
@@ -3,6 +3,7 @@ package com.thoughtworks.rslist.dto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Max;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -18,6 +19,7 @@ public class UserDto {
     private String gender;
 
     @NotNull
+    @Max(18)
     private Integer age;
     private String email;
     private String phone;
